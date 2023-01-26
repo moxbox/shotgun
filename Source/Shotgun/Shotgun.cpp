204ceeacbfb3971a7be31f53f2df23a637957e6b
Zach Moxley <zach@moxl.tech>
2023-01-25 23:24:32 -0500

    Initial commit - Blank Project

---
+++ Source/Shotgun/Shotgun.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Shotgun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Shotgun, "Shotgun" );
---
+++ Source/Shotgun/ShotgunGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShotgunGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOTGUN_API AShotgunGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
